adb489fd42b0dcf2ddc1eb5fb1148981e4df7dff
Nick <90702473+codesshaman@users.noreply.github.com>
2022-08-10 07:51:44 -0400

    Update .pg_backup.sh

--- .pg_backup.sh
+++ .pg_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
- db_name=db_name
- db_user=user_name
- db_host=hostname
- backupfolder=~/DBBackUps
- # Сколько дней хранить файлы
- keep_day=30
- sqlfile=$backupfolder/$db_name-$(date +%y.%m.%d-%H:%M:%S).sql
- mkdir -p $backupfolder
+db_name=db_name
+db_user=user_name
+db_host=hostname
+backupfolder=~/DBBackUps
+# Сколько дней хранить файлы
+keep_day=30
+sqlfile=$backupfolder/$db_name-$(date +%y.%m.%d-%H:%M:%S).sql
+mkdir -p $backupfolder
 
 if pg_dump -U $db_user -h $db_host $db_name > $sqlfile; then
    echo 'Sql dump created'
